54775abd61d80c946b80dc78164ac9e61a2613a6
guru <guru85111@gmail.com>
2024-03-22 17:52:54 +0530

    hero section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import React from 'react';
+import './Header.css';
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+        <div className='logo'>
+            Logo
+        </div>
+        <div className='links'>
+            <a href="#">About us</a>
+            <a href="#">Services</a>
+            <a href="#">Events</a>
+            <a href="#">Contact us</a>
+        </div>
+    </div>
   )
 }
 
